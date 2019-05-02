74d80937a23d4c281a5d80803c471c2c82e7050b
Bloupp <yttrium.zirconis@gmail.com>
2019-05-02 17:47:59 +0200

    Le programme fonctionne !

--- Finalisation TIPE.py
+++ Finalisation TIPE.py
@@ -77,9 +77,10 @@ Ms = 1.989*(10**30)
 m = [3.285*(10**23), 4.867*(10**24), 5.972*(10**24), 6.39*(10**23), 1.898*(10**27), 5.863*(10**26), 8.681*(10**25), 1.024*(10**26)]
 a = [57909227000, 108208475000, 149598262000, 227943824000, 778340821000, 1426666422000, 2870658186000, 4498396441000]
 n_p = [ np.sqrt(G*Ms/(a[i]**3)) for i in range(8) ]
+''' Je vérifie ici les valeurs de n_p, cela convient bien
 T_j = [87.95569, 224.667, 365.256363, 686.885, 4332.01, 10754, 30698 ,60216.8 ]
 T_s = [24*3600*T for T in T_j]
-n_p2= [ 2*pi/T for T in T_s]
+n_p2= [ 2*pi/T for T in T_s]'''
 
 e = [0.20564, 0.0068, 0.0167, 0.0934, 0.0484, 0.0538, 0.0472, 0.0086]
 lpd = [77.43, 131.6, 102.937, 336.1, 14.755, 92.64, 170.92, 44.984]
@@ -103,7 +104,7 @@ def N():
     N=np.zeros((n,n))
     for i in range(n):
         for j in range(i,n):
-            Nij = Fourier(1,i,j)/16
+            Nij = Fourier(1,i,j)/8
             N[i,j],N[j,i] = Nij, Nij
     for i in range(n):
         N[i,i] = 0
@@ -117,7 +118,7 @@ def P():
     P=np.zeros((n,n))
     for i in range(n):
         for j in range(i,n):
-            Pij = Fourier(2,i,j)/16
+            Pij = Fourier(2,i,j)/8
             P[i,j],P[j,i] = Pij, Pij
     for i in range(n):
         P[i,i] = 0
@@ -149,8 +150,8 @@ def Am(l):
                 A[i,j] = -coef_c(i,j)
     return A
 
-def add_func(f,g):
-    return lambda x: f(x) + g(x)
+def mapp(f,T):
+    return [f(t) for t in T]
 
 #Je refait la fonction principale, une fois toutes les fonctions annexes finies, celle-ci prend en entrée une liste de planètes à prendre en compte et renvoie deux listes de fonctions H(t) et L(t)
 def solution_HL(l):
@@ -171,19 +172,29 @@ def solution_HL(l):
     Y = [lambda t : alpha_h[i]*np.cos(D[i,i]*t) + beta_h[i] *np.sin(D[i,i]*t) for i in range(n)]
     Z = [lambda t : alpha_l[i]*np.cos(D[i,i]*t) + beta_l[i] *np.sin(D[i,i]*t)for i in range(n)]
     #On recombine les différentes valeurs pour obtenir H(t) = QY(t), et L(t) = QZ(t)
-    H,L = [lambda x : 0 for i in range(n)],[lambda x : 0 for i in range(n)]
-    for i in range(n):
-        for j in range(n):
-            H[i] = add_func(H[i],lambda x : Q[i,j]*Y[j](x))
-            L[i] = add_func(L[i],lambda x : Q[i,j]*Z[j](x))
+    def h(i):
+        return (lambda t : sum([Q[i,j]*(alpha_h[j]*np.cos(D[j,j]*t) + beta_h[j] *np.sin(D[j,j]*t)) for j in range(n)] ))
+    def l(i):
+        return (lambda t : sum([Q[i,j]*(alpha_l[j]*np.cos(D[j,j]*t) + beta_l[j] *np.sin(D[j,j]*t)) for j in range(n)] ))
+    H = mapp(h,range(n))
+    L = mapp(l,range(n))
     return H,L
 
 H,L = solution_HL(planetes)
 T=np.linspace(-100000*annee,100000*annee,20000)     
 
 plt.figure()
-E2 = [np.sqrt(H[2](t)**2 + L[2](t)**2) for t in T]
-plt.plot(T,E2, lw=2)
-#plt.plot(T,E3, lw=2)
+for i in range(len(planetes)):
+    E = [np.sqrt(H[i](t)**2 + L[i](t)**2) for t in T]
+    plt.plot(T,E, lw=2)
+plt.grid()
 plt.show()
 
+'''Résultats à la main de Le Verrier'''
+
+E_terre=[0.046,0.0473,0.0452,0.0398,0.0316,0.0218,0.0131,0.0109,0.0151,0.0188,0.0187,0.0168,0.0115,0.0047,0.0059,0.0124,0.0173,0.0199,0.0211,0.0188,0.0176,0.0189]
+X=np.linspace(-110000,100000,22)
+
+plt.figure()
+plt.plot(X,E_terre)
+plt.show()
